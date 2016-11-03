012a6cafef6c48ef5324aabaf6e607097e48868e
wsienski <wsienski@ospartner.pl>
2016-11-03 15:09:13 +0100

    RxBinding support multiple click listeners

--- app/src/main/java/iview/wsienski/rxtrip/MainActivity.java
+++ app/src/main/java/iview/wsienski/rxtrip/MainActivity.java
@@ -10,6 +10,11 @@ import android.widget.Toast;
 import com.jakewharton.rxbinding.view.RxView;
 import com.jakewharton.rxbinding.widget.RxTextView;
 
+import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.subscriptions.CompositeSubscription;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,21 +24,25 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.text);
         Button b = (Button) findViewById(R.id.button);
+        Observable<Void> clickObservable = RxView.clicks(b).share();
         EditText editText = (EditText) findViewById(R.id.editText);
+        CompositeSubscription compositeSubscription = new CompositeSubscription();
 
-        bindButton(b);
+
+        bindSubstrictionToKey(clickObservable, compositeSubscription, aVoid -> textView.setText("Click"));
+        bindSubstrictionToKey(clickObservable, compositeSubscription, aVoid -> Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show());
 
         bindEditText(textView, editText);
+    }
+
+    private void bindSubstrictionToKey(Observable<Void> clickObservable, CompositeSubscription compositeSubscription, Action1<Void> click) {
+        Subscription subscription = clickObservable.subscribe(click);
+        compositeSubscription.add(subscription);
     }
 
     private void bindEditText(TextView textView, EditText editText) {
         RxTextView.textChanges(editText)
                 .map(charSequence -> new StringBuilder(charSequence).reverse().toString())
                 .subscribe(charSequence -> textView.setText(charSequence));
-    }
-
-    private void bindButton(Button b) {
-        RxView.clicks(b)
-                .subscribe(aVoid -> Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show());
     }
 }
